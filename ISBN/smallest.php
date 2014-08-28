17322302870c5c09b912f59d6919dac8087f3463
ehime <dodomeki@gmail.com>
2014-08-28 15:12:06 -0700

    293 Characters!!! L/UCFirst not needed

--- ISBN/smallest.php
+++ ISBN/smallest.php
@@ -1,6 +1,6 @@
 <?php error_reporting(1);
 
-// Smallest: Finished in 302 Characters
+// Smallest: Finished in 293 Characters
 // No DB Params, relying on shell execution to shorten item count
 
 // Create
@@ -19,4 +19,4 @@
  * $a[substr(ucwords($e[0]),0,1)].="<a href='/{$e[1]}'>{$e[0]}</a> will raise due to undefined index, but will create on the fly
  */
 
-$z='<br>';$a=[];exec('mysql -N -uu -pp -e"SELECT * FROM d.b"',$o);foreach($o AS$k=>$v){$e=split("\t",$v);$a[lcfirst($e[0][0])].="<a href='/{$e[1]}'>{$e[0]}</a>$z";}foreach(range('A','Z') AS$l)echo(isset($a[$l]))?"<a href='/#$l'>$l</a> ":"$l ";echo$z;foreach($a AS$k=>$v)echo"<a name='$k'>$k</a>$z$v\n";
+$z='<br>';$a=[];exec('mysql -N -uu -pp -e"SELECT * FROM d.b"',$o);foreach($o AS$k=>$v){$e=split("\t",$v);$a[$e[0][0]].="<a href='/{$e[1]}'>{$e[0]}</a>$z";}foreach(range('A','Z') AS$l)echo(isset($a[$l]))?"<a href='/#$l'>$l</a> ":"$l ";echo$z;foreach($a AS$k=>$v)echo"<a name='$k'>$k</a>$z$v\n";
